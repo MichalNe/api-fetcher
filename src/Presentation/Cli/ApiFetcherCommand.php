c945beef6a1b83be1295a0a9af7d01c0e7f03fde
michal.smola <michalsmoladev@gmail.com>
2025-02-28 07:41:03 +0100

    Change output

--- src/Presentation/Cli/ApiFetcherCommand.php
+++ src/Presentation/Cli/ApiFetcherCommand.php
@@ -10,12 +10,16 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncode;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[AsCommand(name: 'api:fetcher', description: 'CLI to fetch data from API')]
 class ApiFetcherCommand extends Command
 {
     public function __construct(
         private readonly ApiFetcherService $apiFetcherService,
+        private readonly SerializerInterface $serializer,
     ) {
         parent::__construct();
     }
@@ -33,7 +37,13 @@ class ApiFetcherCommand extends Command
             $input->getArgument('option')
         );
 
-        dump($result);
+        $output->writeln(
+            $this->serializer->serialize(
+                $result,
+                JsonEncoder::FORMAT,
+                [JsonEncode::OPTIONS => JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT]
+            )
+        );
 
         return Command::SUCCESS;
     }
